9655750f3595778b24f78fb1a4da55399d4529c2
alberto <ajcarracedo@gmail.com>
2017-02-19 21:19:19 +0100

    Versión beta
    Signed-off-by: alberto <ajcarracedo@gmail.com>

--- iteracion_002_barco/iteracion_002_barco.ino
+++ iteracion_002_barco/iteracion_002_barco.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+int rele = 12;
+int pulse = 22;
+int state = 0;
+String s = "";
 
+void setup()
+{
+ Serial3.setTimeout(50);
+ Serial3.begin(57600);
+ Serial.begin(57600);
+ 
+ pinMode(rele,OUTPUT);
+ pinMode(pulse,INPUT);
+ digitalWrite(rele,LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+ if (Serial3.available() > 0) {
+    s = Serial3.readString();
+    Serial.println(s);
+  }
+  
+ state = digitalRead(pulse);
+ 
+ if(state == HIGH)
+ {
+   digitalWrite(rele,HIGH);
+ }
+ 
+ if(state == LOW)
+ {
+   digitalWrite(rele,LOW);    
+
+ }
+
+  delay(100);
 
 }
